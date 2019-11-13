4b12e7e46bc38684198dd61a2fb191021d0cae42
tim ichiba <ichibat@gmail.com>
2019-11-13 21:49:24 +0900

    with client nuxt

---
+++ client/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      <h1>
+        My title
+      </h1>
+      <h2>KTバランスチャートで嚥下機能を改善しましょう！</h2>
+      <h2>このサービスは未完成なので，実際につかったりはしないでください！</h2>
+      <button>スコアを入力．</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
