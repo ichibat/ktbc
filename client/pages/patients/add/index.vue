e7d0c94b3072dbdfc1f77cfb662c7769bff775c1
tim ichiba <ichibat@gmail.com>
2019-11-24 14:55:07 +0900

    Improve

--- client/pages/patients/add/index.vue
+++ client/pages/patients/add/index.vue
@@ -1,30 +1,59 @@
 <template>
-  <v-container fluid>
-    <v-row align="center">
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select :items="items" label="Standard"></v-select>
-      </v-col>
-
-      <!-- <v-col class="d-flex" cols="12" sm="6">
-        <v-select :items="items" filled label="Filled style"></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select :items="items" label="Outlined style" outlined></v-select>
-      </v-col>
-
-      <v-col class="d-flex" cols="12" sm="6">
-        <v-select :items="items" label="Solo field" solo></v-select>
-      </v-col> -->
-    </v-row>
-  </v-container>
+  <div>
+    <v-container fluid>
+      <v-row align="center">
+        <v-col cols="6">
+          <v-select
+            :items="items1"
+            item-text="question"
+            item-value="value"
+            label="(1)食べる意欲"
+            return-object
+            single-line
+          ></v-select>
+          <v-select
+            :items="items2"
+            item-text="question"
+            item-value="value"
+            label="(2)全身状態"
+            return-object
+            single-line
+          ></v-select>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    items: ['Foo', 'Bar', 'Fizz', 'Buzz']
-  })
+  data() {
+    return {
+      items1: [
+        { question: '1.促しや援助しても食べようとしない', value: '1' },
+        { question: '2.促しや援助で少し食べる', value: '2' },
+        { question: '3.促しや援助で半量食べる', value: '3' },
+        { question: '4.促しや援助でほとんど食べる', value: '4' },
+        {
+          question:
+            '5.介助の有無に関わらず食べようとする食べたいと，意思表示する',
+          value: '5'
+        }
+      ],
+      items2: [
+        { question: '1.促しや援助しても食べようとしない', value: '1' },
+        { question: '2.促しや援助で少し食べる', value: '2' },
+        { question: '3.促しや援助で半量食べる', value: '3' },
+        { question: '4.促しや援助でほとんど食べる', value: '4' },
+        {
+          question:
+            '5.介助の有無に関わらず食べようとする食べたいと，意思表示する',
+          value: '5'
+        }
+      ]
+    }
+  }
 }
 </script>
+
 <style></style>
