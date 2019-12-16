10d58c3a9d3808b0414dd20c2f554ce21cf89ef8
tamotsu ichiba <ichibat@gmail.com>
2019-12-17 07:59:02 +0900

    Improves

---
+++ front/components/BaseInputText.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" class="input" :value="value" v-on="listeners">
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    listerners () {
+    return {
+      ...this.$listners,
+      input: event => this.$emit('input', event.target.value)
+      }
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../variables.scss';
+
+.input {
+  width: 100%;
+  padding: 8px 10px;
+  border: 1px solid $vue-blue;
+}
+</style>
--- frontier/pages/index.vue
+++ frontier/pages/index.vue
@@ -1,17 +1,39 @@
 <template>
   <div class="container">
-		<h1>My Todo App!</h1>
-		<TodoList/>
-	</div>
+    <div>
+      <logo />
+      <h1 class="title">
+        frontier
+      </h1>
+      <h2 class="subtitle">
+        My peachy Nuxt.js project
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-   
+    Logo
   }
 }
 </script>
