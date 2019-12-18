a1f755bfbd3b4171abe57b725203b530e18a76f5
tamotsu ichiba <ichibat@gmail.com>
2019-12-18 22:30:09 +0900

    Improves

--- frontier/pages/register/index.vue
+++ frontier/pages/register/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
+  <div class = "l-container">
     <p>ユーザー登録</p>
+    <input v-model="message" placeholder="カルテ番号:">
+<p>カルテ番号は: {{ message }}</p>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      message: ''
+    }
+  }
 }
 </script>
 
 <style>
-
+.l-container {
+  background: lightgray;
+  margin: auto;
+  width: 80%;
+  padding: 10px;
+}
 </style>
