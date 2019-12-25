28bb92cf1ef08248f4480654d789983ea8c2dab1
tamotsu ichiba <ichibat@gmail.com>
2019-12-26 07:54:37 +0900

    PostService.js modification

--- frontier/components/PostService.js
+++ frontier/components/PostService.js
@@ -13,12 +13,39 @@ mongoose
     console.log(err);
   });
 
-class PostService {
-  // Get Posts
+  const url = "/api/posts/";
 
-
-  // Create Post
+  class PostService {
+    //Get Posts
+    static getPosts() {
+      return new Promise(async (resolve, reject) => {
+        try {
+          const res = await axios.get(url);
+          const data = res.data;
+          resolve(
+            data.map(post => ({
+              ...post,
+              createdAt: new Date(post.createdAt)
+            }))
+          );
+        } catch (err) {
+          reject(err);
+        }
+      });
+    }
   
-
-  // Delete Post
-}
+    // Create Post
+    static insertPost(text) {
+      return axios.post(url, {
+        text
+      });
+    }
+  
+    // Delete Post
+    static deletePost(id) {
+      return axios.delete(`${url}${id}`);
+    }
+  }
+  
+  export default PostService;
+  
